e1ea100d3e384aba79a2276722318dcffe30104d
PratY7728 <152558066+PratY7728@users.noreply.github.com>
2024-03-21 15:15:42 +0530

    depoyment

--- frontend/src/component/messages/Message.jsx
+++ frontend/src/component/messages/Message.jsx
@@ -12,6 +12,7 @@ const Message = ({message}) => {
      const chatClassName = fromMe ? 'chat-end' : 'chat-start';
      const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
      const bubbleBgColor =fromMe ? 'bg-blue-500' : "";
+        const p=10;
      const formattedTime = extractTime(message.createdAt);
      const shakeClass = message.shouldShake ? "shake" : "";
   return (
